59cbd40ded9413c36a316d65ed52ba252386a299
Vincent Wei <vincent@minigui.org>
2020-01-10 17:21:48 +0800

    add test programs for compositing schema

---
+++ compositor/static.c
@@ -0,0 +1,124 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/* 
+** $Id: static.c 741 2009-03-31 07:16:18Z weiym $
+**
+** Listing 19.1
+**
+** static.c: Sample program for MiniGUI Programming Guide
+**      Demo for static control.
+**
+** Copyright (C) 2003 ~ 2017 FMSoft (http://www.fmsoft.cn).
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <stdio.h>
+
+#include <minigui/common.h>
+#include <minigui/minigui.h>
+#include <minigui/gdi.h>
+#include <minigui/window.h>
+#include <minigui/control.h>
+
+#define CAPTION "Demo for STATIC Control"
+#define PROMPTA "Double-click me!"
+#define PROMPTB "I am double-clicked. :)"
+
+static void my_notif_proc (HWND hwnd, LINT id, int nc, DWORD add_data)
+{
+    if (nc == STN_DBLCLK)
+        SetWindowText (hwnd, PROMPTB);
+}
+
+static LRESULT StaticDemoWinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HWND hwnd;
+
+    switch (message) {
+        case MSG_CREATE:
+            hwnd = CreateWindow (CTRL_STATIC, PROMPTA, 
+                          WS_VISIBLE | SS_CENTER | SS_NOTIFY, 
+			  50, 55, 95, 200, 20, hWnd, 0);
+            SetNotificationCallback (hwnd, my_notif_proc);
+            return 0;
+
+        case MSG_DESTROY:
+            DestroyAllControls (hWnd);
+            return 0;
+
+        case MSG_CLOSE:
+            DestroyMainWindow (hWnd);
+            PostQuitMessage (hWnd);
+            return 0;
+    }
+
+    return DefaultMainWinProc(hWnd, message, wParam, lParam);
+}
+
+int MiniGUIMain (int argc, const char* argv[])
+{
+    MSG Msg;
+    HWND hMainWnd;
+    MAINWINCREATE CreateInfo;
+
+#ifdef _MGRM_PROCESSES
+    JoinLayer(NAME_DEF_LAYER , "static" , 0 , 0);
+#endif
+
+    CreateInfo.dwStyle = WS_VISIBLE | WS_BORDER | WS_CAPTION;
+    CreateInfo.dwExStyle = WS_EX_NONE;
+    CreateInfo.spCaption = CAPTION;
+    CreateInfo.hMenu = 0;
+    CreateInfo.hCursor = GetSystemCursor(0);
+    CreateInfo.hIcon = 0;
+    CreateInfo.MainWindowProc = StaticDemoWinProc;
+    CreateInfo.lx = 0;
+    CreateInfo.ty = 0;
+    CreateInfo.rx = 320;
+    CreateInfo.by = 240;
+    CreateInfo.iBkColor = COLOR_lightwhite;
+    CreateInfo.dwAddData = 0;
+    CreateInfo.hHosting = HWND_DESKTOP;
+    
+    hMainWnd = CreateMainWindow (&CreateInfo);
+    
+    if (hMainWnd == HWND_INVALID)
+        return -1;
+
+    ShowWindow(hMainWnd, SW_SHOWNORMAL);
+
+    while (GetMessage(&Msg, hMainWnd)) {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
+    MainWindowThreadCleanup (hMainWnd);
+
+    return 0;
+}
+
+#ifdef _MGRM_THREADS
+#include <minigui/dti.c>
+#endif
+
---
+++ compositor/trackbar.c
@@ -0,0 +1,160 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/* 
+** $Id: trackbar.c 741 2009-03-31 07:16:18Z weiym $
+**
+** Listing 26.1
+**
+** trackbar.c: Sample program for MiniGUI Programming Guide
+**      Usage of TRACKBAR control.
+**
+** Copyright (C) 2003 ~ 2017 FMSoft (http://www.fmsoft.cn).
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <minigui/common.h>
+#include <minigui/minigui.h>
+#include <minigui/gdi.h>
+#include <minigui/window.h>
+#include <minigui/control.h>
+
+#ifdef _MGCTRL_TRACKBAR
+
+#define CAPTION "Trackbar" 
+
+static int radius = 0;
+static RECT rcCircle = {0, 60, 240, 300};
+static void my_notif_proc (HWND hwnd, LINT id, int nc, DWORD add_data)
+{
+    if (nc == TBN_CHANGE) {
+        radius = SendMessage (hwnd, TBM_GETPOS, 0, 0);
+        InvalidateRect (GetParent (hwnd), &rcCircle, TRUE);
+    }
+}
+
+static LRESULT TrackBarWinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HWND hwnd;
+    switch (message) {
+    case MSG_CREATE:
+        hwnd = CreateWindow (CTRL_TRACKBAR, 
+                          "", 
+                          WS_VISIBLE | TBS_NOTIFY, 
+                          100, 
+                          10, 10, 210, 50, hWnd, 0);
+
+        SendMessage (hwnd, TBM_SETRANGE, 0, 80);
+        SendMessage (hwnd, TBM_SETLINESIZE, 1, 0);
+        SendMessage (hwnd, TBM_SETPAGESIZE, 10, 0);
+        SendMessage (hwnd, TBM_SETTICKFREQ, 10, 0);
+        SendMessage (hwnd, TBM_SETPOS, radius, 0);
+
+        SetNotificationCallback (hwnd, my_notif_proc);
+        SetFocus (hwnd);
+        break;
+
+    case MSG_PAINT:
+    {
+        HDC hdc = BeginPaint (hWnd);
+
+        ClipRectIntersect (hdc, &rcCircle);
+        Circle (hdc, 100, 180, radius);
+
+        EndPaint (hWnd, hdc);
+        return 0;
+    }
+
+    case MSG_DESTROY:
+        DestroyAllControls (hWnd);
+        return 0;
+
+    case MSG_CLOSE:
+        DestroyMainWindow (hWnd);
+        PostQuitMessage (hWnd);
+        return 0;
+    }
+
+    return DefaultMainWinProc(hWnd, message, wParam, lParam);
+}
+
+int MiniGUIMain (int argc, const char* argv[])
+{
+    MSG Msg;
+    HWND hMainWnd;
+    MAINWINCREATE CreateInfo;
+
+#ifdef _MGRM_PROCESSES
+    JoinLayer(NAME_DEF_LAYER , "trackbar" , 0 , 0);
+#endif
+
+    CreateInfo.dwStyle = WS_CAPTION | WS_BORDER | WS_VISIBLE;
+    CreateInfo.dwExStyle = WS_EX_NONE;
+    CreateInfo.spCaption = CAPTION;
+    CreateInfo.hMenu = 0;
+    CreateInfo.hCursor = GetSystemCursor(IDC_ARROW);
+    CreateInfo.hIcon = 0;
+    CreateInfo.MainWindowProc = TrackBarWinProc;
+    CreateInfo.lx = 0; 
+    CreateInfo.ty = 0;
+    CreateInfo.rx = 240;
+    CreateInfo.by = 320;
+    CreateInfo.iBkColor = GetWindowElementColor (WE_MAINC_THREED_BODY);
+    CreateInfo.dwAddData = 0;
+    CreateInfo.dwReserved = 0;
+    CreateInfo.hHosting = HWND_DESKTOP;
+
+    hMainWnd = CreateMainWindow (&CreateInfo);
+    if (hMainWnd == HWND_INVALID)
+        return -1;
+
+    ShowWindow(hMainWnd, SW_SHOWNORMAL);
+
+    while (GetMessage(&Msg, hMainWnd)) {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
+    MainWindowThreadCleanup (hMainWnd);
+
+    return 0;
+}
+
+#else /* _MGCTRL_TRACKBAR */
+
+int MiniGUIMain (int argc, const char *argv[])
+{
+    fprintf (stderr, "Please enable the support for TRACKBAR control in MiniGUI.\n");
+    return 0;
+}
+
+#endif /* _MGCTRL_TRACKBAR */
+
+#ifdef _MGRM_THREADS
+#include <minigui/dti.c>
+#endif
