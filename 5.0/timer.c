e750536adcb8675187c8a38a89b4fd17427bffd1
Vincent Wei <vincent@minigui.org>
2020-02-22 17:18:53 +0800

    add test code for timer in virtual window

--- 5.0/timer.c
+++ 5.0/timer.c
@@ -18,12 +18,19 @@
 **      DefaultWindowProc
 **      SetTimerEx
 **      KillTimer
+**      IsTimerInstalled
+**      HaveFreeTimer
 **      SetWindowAdditionalData
 **      GetWindowAdditionalData
+**      SetWindowAdditionalData2
+**      GetWindowAdditionalData2
 **      PostQuitMessage
 **      DestroyMainWindow
 **      GetWindowInfo
 **      MainWindowCleanup
+**      CreateVirtualWindow
+**      DestroyVirtualWindow
+**      VirtualWindowCleanup
 **      MSG_TIMER
 **      MSG_IDLE
 **
@@ -79,20 +86,22 @@ static BOOL my_timer_proc (HWND hwnd, LINT id, DWORD ticks)
         ticks_error = timer->ticks_expected - ticks_current;
     }
 
-    assert (ticks_error < 10);
+    if (ticks_error > 5) {
+        _WRN_PRINTF ("Too large error: %lu\n", ticks_error);
+    }
 
     timer->ticks_expected = ticks_current + timer->interv;
     if (ticks_error > timer->ticks_error) {
         timer->ticks_error = ticks_error;
     }
 
-    _MG_PRINTF ("Timer fired: id (%ld), interval (%lu), error (%lu)\n",
-            timer->id, timer->interv, ticks_error);
+    _MG_PRINTF ("Timer fired for window (%s): id (%ld), interval (%lu), "
+            "error (%lu)\n",
+            GetWindowCaption (hwnd), timer->id, timer->interv, ticks_error);
 
     timer->fired_times++;
     timer->wait_for_times--;
     if (timer->wait_for_times == 0) {
-
         info->nr_timers--;
         return FALSE;
     }
@@ -102,16 +111,16 @@ static BOOL my_timer_proc (HWND hwnd, LINT id, DWORD ticks)
 
 static BOOL init_timers (HWND hwnd)
 {
+    int nr_timers = 0;
     struct _test_info* info;
 
     info = calloc (1, sizeof (*info));
     if (info == NULL)
         return FALSE;
 
-    srandom (time(NULL));
     for (int i = 0; i < TABLESIZE (info->timers); i++) {
         info->timers[i].id = (LINT)(&info->timers[i]);
-        info->timers[i].interv = random() % 6000;   // max interval: 60s
+        info->timers[i].interv = random() % 3000;   // max interval: 60s
         if (info->timers[i].interv == 0)
             info->timers[i].interv = 1;
 
@@ -125,7 +134,7 @@ static BOOL init_timers (HWND hwnd)
                 break;
         }
 
-        info->timers[i].wait_for_times = random() % 10;   // max times: 9
+        info->timers[i].wait_for_times = random() % 5;   // max times: 9
         if (info->timers[i].wait_for_times == 0)
             info->timers[i].wait_for_times = 1;
 
@@ -135,8 +144,6 @@ static BOOL init_timers (HWND hwnd)
 
             info->timers[i].ticks_expected = ticks_current +
                 info->timers[i].interv;
-            _WRN_PRINTF ("ticks_expected for timer #%d: %lu\n",
-                    i, info->timers[i].ticks_expected);
 
             info->nr_timers++;
         }
@@ -146,21 +153,34 @@ static BOOL init_timers (HWND hwnd)
         }
     }
 
+    assert (!HaveFreeTimer());
+
+    for (int i = 0; i < TABLESIZE (info->timers); i++) {
+        if (info->timers[i].id) {
+            if (IsTimerInstalled (hwnd, info->timers[i].id))
+                nr_timers++;
+        }
+    }
+
+    assert (nr_timers == info->nr_timers);
+
     SetWindowAdditionalData (hwnd, (DWORD)info);
     return (info->nr_timers == NR_BITS_DWORD);
 }
 
-static void dump_timer_info (HWND hwnd)
+static int dump_timer_info (HWND hwnd)
 {
     struct _test_info *info =
         (struct _test_info *)GetWindowAdditionalData (hwnd);
 
     if (info->nr_timers > 0) {
-        _MG_PRINTF ("Installed timers of window (%s):\n", GetWindowCaption (hwnd));
+        _MG_PRINTF ("Installed timers of window (%s):\n",
+                GetWindowCaption (hwnd));
 
         for (int i = 0; i < TABLESIZE (info->timers); i++) {
             if (info->timers[i].id) {
-                _MG_PRINTF ("Timer #%d: id (%ld), interval (%lu), ticks_expected (%lu), wait_for_times (%d)\n",
+                _MG_PRINTF ("Timer #%d: id (%ld), interval (%lu), "
+                        "ticks_expected (%lu), wait_for_times (%d)\n",
                         i, info->timers[i].id, info->timers[i].interv,
                         info->timers[i].ticks_expected,
                         info->timers[i].wait_for_times);
@@ -187,18 +207,35 @@ static void dump_timer_info (HWND hwnd)
             }
         }
     }
+
+    return info->nr_timers;
 }
 
-static void check_timers (HWND hwnd)
+static void check_timers (HWND hwnd, DWORD ticks)
 {
+    int nr_timers = 0;
+
     struct _test_info *info =
         (struct _test_info *)GetWindowAdditionalData (hwnd);
 
-    _WRN_PRINTF("The number of timers active currently (%lu): %d\n",
-            GetTickCount (), info->nr_timers);
+    _MG_PRINTF("The number of active timers for window (%s): %d, at ticks (%lu)\n",
+            GetWindowCaption (hwnd), info->nr_timers, ticks);
 
-    if (info->nr_timers == 0)
+    for (int i = 0; i < TABLESIZE (info->timers); i++) {
+        if (info->timers[i].id) {
+            if (IsTimerInstalled (hwnd, info->timers[i].id))
+                nr_timers++;
+        }
+    }
+
+    assert (nr_timers == info->nr_timers);
+
+    if (info->nr_timers == 0 &&
+            // we use dwAddData2 to check whether the message thread
+            // for the virtual window has exited.
+            GetWindowAdditionalData2 (hwnd) == (DWORD)-1) {
         PostQuitMessage (hwnd);
+    }
 }
 
 static void clean_timers (HWND hwnd)
@@ -209,10 +246,48 @@ static void clean_timers (HWND hwnd)
     assert (info);
     free (info);
 
-    _MG_PRINTF("data for test freed\n");
+    _MG_PRINTF ("Test data of window (%s) freed\n", GetWindowCaption (hwnd));
 
     SetWindowAdditionalData (hwnd, 0);
     /* all timers will be killed by DestroyMainWindow() */
+}
+
+enum {
+    CMD_VIRTWND_FAILED = 0,
+    CMD_VIRTWND_CREATED,
+    CMD_VIRTWND_DESTROYED,
+    CMD_VIRTWND_QUITED,
+};
+
+static void on_command (HWND hWnd, WPARAM cmd_id, LPARAM arg)
+{
+    switch (cmd_id) {
+        case CMD_VIRTWND_FAILED:
+            _ERR_PRINTF ("Failed to create the virtual window\n");
+            break;
+
+        case CMD_VIRTWND_CREATED:
+            _WRN_PRINTF ("The virtual window has been created: %p\n",
+                    (void*)arg);
+            SetWindowAdditionalData2 (hWnd, arg);
+            break;
+
+        case CMD_VIRTWND_DESTROYED:
+            _WRN_PRINTF ("The virtual window has been destroyed: %p\n",
+                    (void*)arg);
+            SetWindowAdditionalData2 (hWnd, 0);
+            break;
+
+        case CMD_VIRTWND_QUITED:
+            _WRN_PRINTF ("The message thread for the virtual window exited: %p\n",
+                   (void*)arg);
+            SetWindowAdditionalData2 (hWnd, (DWORD)-1);
+            break;
+
+        default:
+            _WRN_PRINTF ("Unknown command identifier: %d\n", (int)cmd_id);
+            break;
+    }
 }
 
 static LRESULT
@@ -220,17 +295,24 @@ TestWinProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message) {
     case MSG_CREATE:
-        if (init_timers (hWnd))
+        if (init_timers (hWnd)) {
+            SetWindowAdditionalData2 (hWnd, (DWORD)-1);
             return 0;
+        }
         return -1;
 
+    case MSG_COMMAND:
+        on_command (hWnd, wParam, lParam);
+        break;
+        
     case MSG_IDLE:
-        check_timers (hWnd);
+        check_timers (hWnd, (DWORD)wParam);
         break;
 
     case MSG_TIMER:
         if (!my_timer_proc (hWnd, (LINT)wParam, (DWORD)lParam)) {
             KillTimer (hWnd, (LINT)wParam);
+            assert (HaveFreeTimer ());
         }
         break;
         
@@ -242,6 +324,44 @@ TestWinProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return DefaultWindowProc (hWnd, message, wParam, lParam);
 }
 
+#ifdef _MGHAVE_VIRTUAL_WINDOW
+static void* test_timer_in_message_thread (void* arg)
+{
+    MSG Msg;
+    HWND hMainWnd = (HWND)arg;
+    HWND hVirtWnd = CreateVirtualWindow (HWND_DESKTOP,
+            "Virtual Window for timer test", 0, TestWinProc, 0);
+
+    if (hVirtWnd == HWND_INVALID) {
+        SendNotifyMessage (hMainWnd, MSG_COMMAND, CMD_VIRTWND_FAILED, 0);
+        return NULL;
+    }
+
+    SendNotifyMessage (hMainWnd, MSG_COMMAND, CMD_VIRTWND_CREATED,
+            (LPARAM)hVirtWnd);
+
+    dump_timer_info (hVirtWnd);
+
+    while (GetMessage (&Msg, hVirtWnd)) {
+        DispatchMessage (&Msg);
+    }
+
+    if (dump_timer_info (hVirtWnd) > 0) {
+        _ERR_PRINTF ("The message loop quited unexpectedly\n");
+    }
+
+    DestroyVirtualWindow (hVirtWnd);
+
+    SendNotifyMessage (hMainWnd, MSG_COMMAND, CMD_VIRTWND_DESTROYED,
+            (LPARAM)hVirtWnd);
+
+    VirtualWindowCleanup (hVirtWnd);
+
+    SendNotifyMessage (hMainWnd, MSG_COMMAND, CMD_VIRTWND_QUITED, 0);
+    return NULL;
+}
+#endif  /* defined _MGHAVE_VIRTUAL_WINDOW */
+
 static int test_timer_in_gui_thread (void)
 {
     MSG Msg;
@@ -251,7 +371,7 @@ static int test_timer_in_gui_thread (void)
     CreateInfo.dwStyle = 
         WS_VISIBLE | WS_BORDER | WS_CAPTION;
     CreateInfo.dwExStyle = WS_EX_NONE;
-    CreateInfo.spCaption = "timer tester";
+    CreateInfo.spCaption = "Main window for timer test";
     CreateInfo.hMenu = 0;
     CreateInfo.hCursor = GetSystemCursor(0);
     CreateInfo.hIcon = 0;
@@ -268,23 +388,35 @@ static int test_timer_in_gui_thread (void)
     if (hMainWnd == HWND_INVALID)
         return -1;
 
+#ifdef _MGHAVE_VIRTUAL_WINDOW
+    pthread_t th;
+
+    if (CreateThreadForMessaging (&th, NULL, test_timer_in_message_thread,
+            (void*)hMainWnd, TRUE, 0)) {
+
+        _ERR_PRINTF ("Failed to create message thread\n");
+        return -1;
+    }
+#endif  /* defined _MGHAVE_VIRTUAL_WINDOW */
+
     dump_timer_info (hMainWnd);
 
     ShowWindow (hMainWnd, SW_SHOWNORMAL);
-
-    while (GetMessage(&Msg, hMainWnd)) {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
+    while (GetMessage (&Msg, hMainWnd)) {
+        TranslateMessage (&Msg);
+        DispatchMessage (&Msg);
     }
 
-    dump_timer_info (hMainWnd);
+    if (dump_timer_info (hMainWnd) > 0) {
+        _ERR_PRINTF ("The message loop quited unexpectedly\n");
+    }
 
     DestroyMainWindow (hMainWnd);
 
     /* The dwAddData of this window has been set to zero in clean_timers.
        We use GetWindowInfo to check the dwAddData of this window,
        because the window is to be deleted, GetWindowAdditionalData will
-       always returns 0. */
+       always return 0. */
     const WINDOWINFO *win_info = GetWindowInfo (hMainWnd);
     assert (win_info->dwAddData == 0);
 
@@ -297,6 +429,8 @@ int MiniGUIMain (int argc, const char* argv[])
     int retval;
 
     JoinLayer (NAME_DEF_LAYER , "timer" , 0 , 0);
+
+    srandom (time(NULL));
 
     for (int i = 0; i < 3; i++) {
         retval = test_timer_in_gui_thread ();
